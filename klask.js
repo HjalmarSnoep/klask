2613e95f53af9e7fc88630068f1f4bd28e5c22f8
HjalmarSnoep <26629952+HjalmarSnoep@users.noreply.github.com>
2020-03-15 00:50:33 +0100

    Update klask.js

--- klask.js
+++ klask.js
@@ -1,1 +1,27 @@
-window.alert(" alert awareness.. ");
+(function(){
+  window.addEventListener("load",this.init.bind(this));
+  
+  var model={}; // the game model
+  var view={}; // the dom.
+  
+  function setAttr(el, init)
+  {
+    for(var all in init)
+    {
+      el.setAttributeNS("http://www.w3.org/2000/svg",all, init[all]);
+    }
+  }
+  function create(tag, init)
+  {
+    var el=document.createElementNS("http://www.w3.org/2000/svg", tag);
+    setAttr(el, init);
+    return el; 
+  }
+  function init()
+  {
+     // create elements we are going to use..
+     view.svg=create("svg");
+     setAttr(view.svg,{width:500,height:500,viewBox:"0 0 500 500"});
+     document.body.appendChild(view.svg);
+  }
+})();

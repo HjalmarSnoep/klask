a0ae52b9c398a9371e9b245a91f6a6f858df9b2a
HjalmarSnoep <26629952+HjalmarSnoep@users.noreply.github.com>
2020-03-15 01:04:11 +0100

    Update klask.js

--- klask.js
+++ klask.js
@@ -8,7 +8,7 @@
   {
     for(var all in init)
     {
-      el.setAttributeNS("http://www.w3.org/2000/svg",all, init[all]);
+      el.setAttribute(all, init[all]);
     }
   }
   function create(tag, init)
@@ -21,7 +21,11 @@
   {
      // create elements we are going to use..
      view.svg=create("svg");
-     setAttr(view.svg,{width:500,height:500,viewBox:"0 0 500 500"});
+     setAttr(view.svg,{width:"100%",height:"100%",viewBox:"0 0 500 500",preserveAspectRatio:"xMidYMid meet"});
      document.body.appendChild(view.svg);
+   view.walls=create("rect",{x:0,y:0,width:500,height:500,fill:"#eee"})
+     view.svg.appendChild(view.walls);
+   view.hero=create("circle",{cx:15,cy:15,r:15,fill:"#f00"})
+     view.svg.appendChild(view.hero);
   }
 })();
